2c35169b97b2afbf19ac4224a6ba3e6878b758c2
Ana Paula Amora <163358904+anapaulaamora@users.noreply.github.com>
2024-09-24 15:57:13 -0300

    Arma.java

---
+++ Arma.java
@@ -0,0 +1,117 @@
+import java.util.Scanner;
+
+
+public class Arma{
+
+
+    private String arma;
+    private int constante = 2;
+    private float dano;
+
+
+    Dados dados = new Dados();
+
+
+     public void adicionarArma(int destreza, int forca){
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+        System.out.println("Escolha sua superpoder:");
+        System.out.println("1. ----- ---------- ");
+        System.out.println("2. ----- -----------");
+        System.out.println("3. ----------");
+        System.out.println("4. ------------ -- ----- - -----");
+        System.out.print("Escolha o número da superpoder: ");
+
+
+
+
+        int escolhaArma = scanner.nextInt();
+
+
+
+
+        switch (escolhaArma) {
+            case 1:
+                this.arma = " Ficar invisivel (leve)";
+                System.out.println("--------------SEU PODER------------");
+                calcularDanoLeve(destreza);
+           
+
+
+                break;
+            case 2:
+                this.arma = " Dedos raios-laser (Pesada)";
+                System.out.println("-------------SEU PODER-------------");
+                calcularDanoPesado(forca);
+
+
+
+
+                break;
+            case 3:
+              this.arma = " Superforca (leve)";
+              System.out.println("-------------SEU PODER-----------------");
+              calcularDanoLeve(destreza);
+               
+
+
+              break;
+            case 4:
+               this.arma = " Especialista em armas - fuzil (Pesada)";
+              System.out.println("-----------SEU PODER---------------");
+              calcularDanoPesado(forca);
+
+
+              break;
+            default:
+                System.out.println("------------Voce nao e um super, ira receber uma adaga--------------");
+                this.arma = "Adaga (Leve)";
+                break;
+
+
+
+
+        }
+
+
+
+
+        System.out.println("poder escolhida: " + arma);
+        System.out.println("Dano calculado: " + dano);
+
+
+
+
+
+
+    }
+
+
+    private void calcularDanoLeve(int destreza) {
+        int d6 = dados.rolarD6();
+        int d4 = dados.rolarD4();
+        dano = d6+ d6 + d4 + destreza + constante;
+    }
+
+
+    private void calcularDanoPesado(int forca) {
+        int d12 = dados.rolarD12();
+        dano = d12 + (1.5f * forca) + constante;
+
+
+    }
+
+
+
+
+    @Override
+    public String toString(){
+        return "poder: " +arma;
+   
+}
+
+
+}
