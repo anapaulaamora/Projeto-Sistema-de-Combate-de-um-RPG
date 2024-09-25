ba4eb38302f5246366f549bc4b64b99d58aabd20
IngridImbiriba <imbiribaingrid@gmail.com>
2024-09-24 22:51:04 -0300

    Update Vida.java

--- Vida.java
+++ Vida.java
@@ -1,52 +1,32 @@
 public class Vida {
+
     Dados dados = new Dados();
 
-
-
-
     private int pontosVida;
-    private int constante = 2;
+    private int constante = 5;
+    private int pontosVidaMaximos;
+    private int novaVida;
 
 
-    public Vida() {
-        vida();  // Chama o método vida() no construtor
+    public void vida(){
+
+        int d6 = dados.rolarD6();
+
+     pontosVida = d6 + d6 + d6 + constante;
+
     }
 
-
-    // Método que calcula os pontos de vida
-    public void vida() {
-        int d6 = rolarD6();  // Certifique-se que o método rolarD6 exista
-        pontosVida = d6 + d6 + d6 + constante;
-    }
-
-
-    // Método fictício para rolar um D6 (substitua conforme necessário)
-    public int rolarD6() {
-        // Simulando o rolar de um dado de 6 lados
-        return (int) (Math.random() * 6) + 1;
-    }
-
-
-
-
-     public int getPontosVida() {
+    public int getPontosVida() { 
         return pontosVida;
     }
-
-
-
-
-
-
-
-
-
-
-
+    public void setVida(int novaVida) {
+        this.pontosVida = novaVida;
+    }
 
     @Override
     public String toString(){
         return "PV: " +pontosVida;
-   
+    
 }
+
 }
