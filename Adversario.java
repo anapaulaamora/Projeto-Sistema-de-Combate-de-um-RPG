288b160eccef5af6f2e65805581194ea0462688b
IngridImbiriba <imbiribaingrid@gmail.com>
2024-09-24 22:46:24 -0300

    Update Adversario.java

--- Adversario.java
+++ Adversario.java
@@ -1,185 +1,81 @@
-import java.util.Scanner;
-
+import java.util.Random;
 
 public class Adversario {
-    Scanner scanner = new Scanner(System.in);
     private String adversario;
     private int vida;
     private int dano;
     private int agilidade;
     private int defesa;
 
-
-    Dados dados = new Dados();
-
-
-    public void primeirosViloes(){
-
-
-           System.out.println("Historia.....");
-           System.out.println();
-           System.out.println("Pressione Enter para continuar...");
-           scanner.nextLine();
-
-
-           System.out.println("Escolha um dos tres para atacar:");
-           System.out.println("1. ------");
-           System.out.println("2. --------");
-           System.out.println("3. ------");
-           System.out.println("Escolha o numero do seu adversario: ");
-
-
-
-
-           int escolhaAdversario1 = scanner.nextInt();
-
-
-           switch(escolhaAdversario1){
-
-
-              case 1:
-              this.adversario = "Kimiko";
-              System.out.println("voce atacou a kimiko");
-              this.vida = 15;    
-              this.agilidade = 8;
-              this.defesa = 9;
-              this.dano = 8;
- 
- 
-              break;
-              case 2:
-              this.adversario = "Leitinho";
-              System.out.println("voce atacou o leitinho! ");
-              this.vida = 10;
-              this.agilidade = 7;
-              this.defesa = 5;
-              this.dano = 7;
- 
- 
-              break;
-              case 3:
-              this.adversario = "Frances";
-              System.out.println("voce atacou o frances! ");
-              this.vida = 15;    
-              this.agilidade = 8;
-              this.defesa = 8;
-              this.dano = 5;
-              break;
-
-
-              default:
-              System.out.println("Opção inválida.");
-              return;
-
-
-           }
-       
-           exibirEstatisticas();
-
-
+    public Adversario(String adversario, int vida, int dano, int agilidade, int defesa) {
+        this.adversario = adversario;
+        this.vida = vida;
+        this.dano = dano;
+        this.agilidade = agilidade;
+        this.defesa = defesa;
     }
 
+    public int getDano() {
+        return dano;
+    }
 
-    public void segundosViloes(){
+    public int getAgilidade() {
+        return agilidade;
+    }
 
+    public int getDefesa() {
+        return defesa;
+    }
 
-        System.out.println("Historia.....");
-        System.out.println();
-        System.out.println("Pressione Enter para continuar...");
-        scanner.nextLine();
+    public int getVida() {
+        return vida;
+    }
 
+    public void setVida(int novaVida) {
+        this.vida = novaVida;
+    }
 
-        System.out.println("Escolha um dos dois para atacar:");
-        System.out.println("1. ------");
-        System.out.println("2. ---------");
-        System.out.println("Escolha o numero do seu adversario: ");
+    public String getNome() {
+        return adversario;
+    }
 
+    public static Adversario gerarAdversario(int rodada) {
+        Random random = new Random();
+        int escolha;
 
-
-
-        int escolhaAdversario2 = scanner.nextInt();
-
-
-        switch(escolhaAdversario2){
-
-
-            case 1:
-            this.adversario = "Hughie";
-            System.out.println("voce atacou o Hughie");
-            this.vida = 11;    
-            this.agilidade = 8;
-            this.defesa = 7;
-            this.dano = 6;
-            break;
-
-
-
-
-            case 2:
-            this.adversario = "Luz Estrela";
-            System.out.println("voce atacou a Luz estrela");
-            this.vida = 16;    
-            this.agilidade = 8;
-            this.defesa = 10;
-            this.dano = 10;
-            break;
-
-
-            default:
-            System.out.println("Opção inválida.");
-            return;
-
-
+        if (rodada == 0) {
+            escolha = random.nextInt(3);
+            if (escolha == 0) {
+                return new Adversario("Kimiko", 18, 6, 3, 2);
+            } else if (escolha == 1) {
+                return new Adversario("Leitinho", 19, 5, 2, 3);
+            } else {
+                return new Adversario("Françe", 17, 4, 2, 1);
+            }
+        } else if (rodada == 1) {
+            escolha = random.nextInt(2);
+            if (escolha == 0) {
+                return new Adversario("Hughie", 20, 5, 5, 3);
+            } else {
+                return new Adversario("Luz Estrela", 21, 6, 14, 3);
+            }
+        } else if (rodada == 2) {
+            return new Adversario("Billy Brutos", 22, 7, 4, 4);
+        } else {
+            throw new IllegalArgumentException("Rodada inválida: " + rodada);
         }
-
-
-        exibirEstatisticas();
-
-
     }
-
-
-
-
-    public void ultimoVilao(){
-
-
-        System.out.println("Historia.....");
-        System.out.println();
-        System.out.println("Pressione Enter para continuar...");
-        scanner.nextLine();
-
-
-        System.out.println("Voce vai enfrentar Bily Bruto");
-        this.adversario = "Bruto";
-        this.vida = 15;    
-        this.agilidade = 10;
-        this.defesa = 9;
-        this.dano = 12;
-
-
-        exibirEstatisticas();
-
-
-    }
-
-
-
-
-    public void exibirEstatisticas() {
-        System.out.println("Adversário: " + adversario);
-        System.out.println("Vida: " + vida);
-        System.out.println("Agilidade: " + agilidade);
-        System.out.println("Defesa: " + defesa);
-        System.out.println("Dano: " + dano);
-    }
-
 
     @Override
     public String toString() {
-        return "Adversario: " + adversario + "\n Vida: " + vida + "\n Agilidade: " + agilidade + "\n Defesa: " + defesa + " \n Dano: " + dano;
+        return "Adversario: " + adversario + 
+               "\nVida: " + vida + 
+               "\nAgilidade: " + agilidade + 
+               "\nDefesa: " + defesa + 
+               "\nDano: " + dano;
     }
 }
+
 
 
 
