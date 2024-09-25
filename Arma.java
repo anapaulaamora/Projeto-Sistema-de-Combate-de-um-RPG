5a65ed686eefb6d239c357684241e0551c7322ba
IngridImbiriba <imbiribaingrid@gmail.com>
2024-09-24 22:47:11 -0300

    Update Arma.java

--- Arma.java
+++ Arma.java
@@ -1,21 +1,16 @@
 import java.util.Scanner;
 
-
 public class Arma{
 
-
     private String arma;
-    private int constante = 2;
+    private int constanteLeve = 2;
+    private int constantePesada = 3;
     private float dano;
-
 
     Dados dados = new Dados();
 
-
      public void adicionarArma(int destreza, int forca){
         Scanner scanner = new Scanner(System.in);
-
-
 
 
         System.out.println("Escolha sua superpoder:");
@@ -26,11 +21,7 @@ public class Arma{
         System.out.print("Escolha o número da superpoder: ");
 
 
-
-
         int escolhaArma = scanner.nextInt();
-
-
 
 
         switch (escolhaArma) {
@@ -38,16 +29,13 @@ public class Arma{
                 this.arma = " Ficar invisivel (leve)";
                 System.out.println("--------------SEU PODER------------");
                 calcularDanoLeve(destreza);
-           
-
+            
 
                 break;
             case 2:
                 this.arma = " Dedos raios-laser (Pesada)";
                 System.out.println("-------------SEU PODER-------------");
                 calcularDanoPesado(forca);
-
-
 
 
                 break;
@@ -57,13 +45,11 @@ public class Arma{
               calcularDanoLeve(destreza);
                
 
-
               break;
             case 4:
                this.arma = " Especialista em armas - fuzil (Pesada)";
               System.out.println("-----------SEU PODER---------------");
               calcularDanoPesado(forca);
-
 
               break;
             default:
@@ -72,46 +58,35 @@ public class Arma{
                 break;
 
 
-
-
         }
-
-
 
 
         System.out.println("poder escolhida: " + arma);
         System.out.println("Dano calculado: " + dano);
 
-
-
-
-
-
     }
 
+    public float getDano() {
+        return dano;
+    }
 
     private void calcularDanoLeve(int destreza) {
         int d6 = dados.rolarD6();
         int d4 = dados.rolarD4();
-        dano = d6+ d6 + d4 + destreza + constante;
+        dano = d6+ d6 + d4 + destreza + constanteLeve;
     }
-
 
     private void calcularDanoPesado(int forca) {
         int d12 = dados.rolarD12();
-        dano = d12 + (1.5f * forca) + constante;
-
+        dano = d12 + (1.5f * forca) + constantePesada;
 
     }
-
-
 
 
     @Override
     public String toString(){
         return "poder: " +arma;
-   
+    
 }
-
 
 }
