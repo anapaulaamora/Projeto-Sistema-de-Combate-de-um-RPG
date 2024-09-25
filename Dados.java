a57878df3c15ce3a1021d2b2b5888560b1662a10
IngridImbiriba <imbiribaingrid@gmail.com>
2024-09-24 22:48:56 -0300

    Update Dados.java

--- Dados.java
+++ Dados.java
@@ -1,24 +1,17 @@
 import java.util.Random;
 
-
 class Dados {
     private Random random = new Random();
-
 
     public int rolarD6() {
         return random.nextInt(6) + 1; // Gera um número entre 1 e 6
     }
 
-
     public int rolarD12() {
         return random.nextInt(12) + 1; // Gera um número entre 1 e 12
     }
 
-
     public int rolarD4() {
         return random.nextInt(4) + 1; // Gera um número entre 1 e 4
-    }
-public float dano(){
-        return random.nextFloat() * 5.0f + 15.0f;
     }
 }
